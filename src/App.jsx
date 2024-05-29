34b41ea7ea683729b6a4de7151b11ab0e580462e
Frosty <allavoine.marine@gmail.com>
2024-05-29 13:02:43 +0200

    test ghp

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import ThemeSwitcher from "./Components/ThemeSwitcher";
 import Home from "./Pages/Home";
-//import Error from "./Pages/Error";
+import Error from "./Pages/Error";
 import ScrollToTopButton from "./Components/ScrollToTopButton";
 import { LanguageProvider } from "./context/LanguageContext";
 import LanguageSwitcher from "./Components/LanguageSwitcher";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <LanguageProvider>
         <ThemeSwitcher />
         <Header />
@@ -22,12 +22,16 @@ const App = () => {
               path="/"
               element={<Home />}
             />
+            <Route  
+              path="*"
+              element={<Error />}
+            />
           </Routes>
         </AnimatePresence>
         <ScrollToTopButton />
         <Footer />
       </LanguageProvider>
-    </Router>
+    </BrowserRouter>
   );
 };
 
