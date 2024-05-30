35e79492c30bdd3544900954ffad3905c3d74ade
Frosty <allavoine.marine@gmail.com>
2024-05-30 16:41:17 +0200

    Deploiement

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- // base: '/portfolio_Frosty/', // Spécifie le chemin de base pour le déploiement sur GitHub Pages
+
+  base: process.env.NODE_ENV === 'production' ? "/portfolio_Frosty" : "/", // Spécifie le chemin de base pour le déploiement sur GitHub Pages
   plugins: [react()],
 })
